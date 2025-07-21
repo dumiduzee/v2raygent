bc170ce4ea0adcd9472313edb03c210f9920bf9a
dumiduzee <dumidulk@outlook.com>
2025-07-21 18:39:35 +0530

    feat:added health router

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter
 
 auth_router = APIRouter(tags=["Auth"])
+
+
+#Health router
+@auth_router.get("/health")
+def index():
+    return "I'm healthy"
--- src/user/router.py
+++ src/user/router.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
 user_router = APIRouter(tags=["User"])
+
+#Health router
+@user_router.get("/health")
+def index():
+    return "I'm healthy"
