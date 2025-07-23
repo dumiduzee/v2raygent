30a43c97b74307e4790741a4054554030604a89b
dumiduzee <dumidulk@outlook.com>
2025-07-23 20:48:04 +0530

    add: cors plocie added

--- src/main.py
+++ src/main.py
@@ -5,12 +5,22 @@ from src.auth.router import auth_router
 from src.user.Exceptions import UserExceptions
 from src.user.router import user_router
 from src.setting import setting
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="V2raygent",
     description="Ai powered v2ray agent"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Accept requests from any origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Accept all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Accept all headers
+)
+
 
 #router definition
 app.include_router(prefix="/v1/auth",router=auth_router)
